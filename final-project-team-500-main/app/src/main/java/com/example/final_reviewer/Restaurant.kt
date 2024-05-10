0b20ff3375fa4006ff26ec0f5be6f4ff02e81033
jasontrannn <143853268+jasontrannn@users.noreply.github.com>
2024-05-09 23:39:02 -0700

    Add files via upload

---
+++ final-project-team-500-main/app/src/main/java/com/example/final_reviewer/Restaurant.kt
@@ -0,0 +1,41 @@
+package com.example.final_reviewer
+
+import android.media.Rating
+
+public class Restaurant {
+    var id : Int = 0
+    var name: String = ""
+    var address: String = ""
+    var comments: String = ""
+    var rating: String = ""
+    var yelpUrl: String = ""
+    var imageUrl: String = ""
+    //val rating: Double,
+    //val eatAgain: Boolean
+
+    constructor(id: Int, name: String, address: String, comments: String, rating: String, yelpUrl: String, imageUrl: String) {
+        this.id
+        this.name = name
+        this.address = address
+        this.comments = comments
+        this.rating = rating
+        this.yelpUrl = yelpUrl
+        this.imageUrl = imageUrl
+    }
+
+    constructor(){}
+    constructor(name: String, address: String, rating: String, comments: String) {
+        this.name = name
+        this.address = address
+        this.rating = rating
+        this.comments = comments
+    }
+
+    public fun getRestaurantID() : Int { return id }
+    public fun getRestaurantName() : String { return name }
+    public fun getRestaurantAddress() : String { return address }
+    public fun getRestaurantComments() : String { return comments }
+    public fun getRestaurantRating() : String { return rating }
+    public fun getRestaurantYelpUrl() : String { return yelpUrl }
+    public fun getRestaurantImageUrl() : String { return imageUrl }
+}
